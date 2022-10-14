e2d06f1c0e9b0581c41c2212b8b05bf2bb5195e6
sebous <sebous@gmail.com>
2022-10-14 18:30:18 +0200

    rm d3 tests

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -139,22 +139,3 @@ pub fn part_two(input: &str) -> u32 {
 fn main() {
     aoc::solve!(&aoc::read_file("inputs", 3), part_one, part_two)
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_part_one() {
-        use aoc::read_file;
-        let input = read_file("examples", 3);
-        assert_eq!(part_one(&input), 12);
-    }
-
-    #[test]
-    fn test_part_two() {
-        use aoc::read_file;
-        let input = read_file("examples", 3);
-        assert_eq!(part_two(&input), 0);
-    }
-}
